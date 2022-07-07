6ac44e8fe75590b7422cec2dc1f73b69389cdeb9
Ranjeet <Ranjeet127710@gmail.com>
2022-07-07 21:51:20 +0530

    first change

---
+++ PalindromeString.java
@@ -0,0 +1,20 @@
+class PalindromeString{
+
+   public static void main(String args[]){
+
+         String s="Ranjet";
+          boolean c=true;
+           for(int i=0;i<s.length()/2;i++){
+
+                      if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+
+                        c=false;
+                        break;
+                     }
+           }
+             if(c==true){
+                 System.out.print("P");
+            }else{System.out.print("N P");}
+  }
+
+}
